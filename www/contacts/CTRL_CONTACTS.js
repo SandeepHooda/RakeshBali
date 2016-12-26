e50ff32c0c1e3462006f72b5e310c7beed829d4b
SandeepHooda <sonu.hooda@gmail.com>
2016-12-26 20:47:24 +0530

    call feature

--- www/contacts/CTRL_CONTACTS.js
+++ www/contacts/CTRL_CONTACTS.js
@@ -1,5 +1,9 @@
 APP.CONTROLLERS.controller ('CTRL_CONTACTS',['$scope',
     function($scope){
-		$scope.name = "Hooda"
+			
+		$scope.call = function(){
+			//cordova plugin add https://github.com/Rohfosho/CordovaCallNumberPlugin.git
+			window.plugins.CallNumber.callNumber(function(){}, function(){}, "9216411835", true);
+		}	
 	}
 ])

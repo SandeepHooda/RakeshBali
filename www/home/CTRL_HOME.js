c334cd6648c2dbd9f100a03d20a7977f1a9696b3
SandeepHooda <sonu.hooda@gmail.com>
2016-12-26 18:12:35 +0530

    CommitWithCollapsable List
    TestCode

--- www/home/CTRL_HOME.js
+++ www/home/CTRL_HOME.js
@@ -1,6 +1,28 @@
 APP.CONTROLLERS.controller ('CTRL_HOME',['$scope',
     function($scope){
-	$scope.name = "Sandeep"
+	$scope.groups = [];
+	  for (var i=0; i<10; i++) {
+	    $scope.groups[i] = {
+	      name: i,
+	      items: [],
+	      show: false
+	    };
+	    for (var j=0; j<3; j++) {
+	      $scope.groups[i].items.push(i + '-Sandeep' + j);
+	    }
+	  }
+	  
+	  /*
+	   * if given group is the selected group, deselect it
+	   * else, select the given group
+	   */
+	  $scope.toggleGroup = function(group) {
+	    group.show = !group.show;
+	  };
+	  $scope.isGroupShown = function(group) {
+	    return group.show;
+	  };
+	  
 		
 	}
 ])
